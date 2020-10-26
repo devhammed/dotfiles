83d2f1b2d5ca5d76aa0622362676c6857ee4ab2a
Hammed Oyedele <itz.harmid@gmail.com>
2020-10-26 12:02:13 +0100

    feat: add more configs

---
+++ .local/share/albert/org.albert.extension.python/modules/spotlight.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""
+X11 window switcher - list and activate windows.
+"""
+
+import subprocess
+from shutil import which
+from collections import namedtuple
+
+from albertv0 import *
+
+Window = namedtuple('Window', [
+    'wid',
+    'desktop',
+    'wm_class',
+    'host',
+    'wm_name'
+])
+
+__iid__ = 'PythonInterface/v0.1'
+__prettyname__ = 'Spotlight'
+__version__ = '1.0'
+__author__ = 'Hammed Oyedele'
+__dependencies__ = ['wmctrl']
+
+if which('wmctrl') is None:
+    raise Exception('"wmctrl" is not in $PATH.')
+
+
+def getWindowTitle(name: str):
+    names = {
+        'Vlc': 'VLC Media Player',
+        'Code': 'Visual Studio Code',
+    }
+
+    sanitizedTitle = name.title().split('.')[-1].replace('-', ' ')
+
+    return names.get(sanitizedTitle, sanitizedTitle)
+
+
+def getWindowIcon(win_code: str):
+    icons = {
+        'code': 'com.visualstudio.code',
+        'gnome-terminal-server': 'org.gnome.Terminal',
+    }
+
+    return iconLookup(icons.get(win_code, win_code))
+
+
+def handleQuery(query):
+    results = []
+    stripped = query.string.strip().lower()
+
+    for line in subprocess.check_output(['wmctrl', '-l', '-x']).splitlines():
+        win = Window(*[token.decode() for token in line.split(None, 4)])
+        win_code = win.wm_class.split('.')[0]
+
+        if win.wm_name != 'albert — Albert' and win.desktop != '-1' and stripped in win_code.lower():
+            results.append(
+                Item(
+                    id=f'{__prettyname__}_{win.wm_class}',
+                    icon=getWindowIcon(win_code),
+                    text=getWindowTitle(win.wm_class),
+                    subtext=win.wm_name,
+                    actions=[
+                        ProcAction(
+                            'Switch Window',
+                            ['wmctrl', '-i', '-a', win.wid]
+                        ),
+                        ProcAction(
+                            'Close the window gracefully.',
+                            ['wmctrl', '-c', win.wid]
+                        ),
+                    ],
+                ),
+            )
+
+    return results
